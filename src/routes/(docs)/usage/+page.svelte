cab63383c220f1a46fc38a8c9aa8929a5ab73c63
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-08-15 13:07:16 -0400

    fix typo

--- src/routes/(docs)/usage/+page.svelte
+++ src/routes/(docs)/usage/+page.svelte
@@ -10,6 +10,6 @@
 <h2>Notice</h2>
 <p>
 	I kindly ask that you do not make an unreasonable number of requests to the APIs in order to
-	ensure the Penylo buisness model is sustainable for everyone. Hosting Penylo isn't free, but
+	ensure the Penylo business model is sustainable for everyone. Hosting Penylo isn't free, but
 	currently it's not a burden. I would like to keep it that way so I can make it free for everyone.
 </p>
