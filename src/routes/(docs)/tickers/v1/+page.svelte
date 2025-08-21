8b47c8207d5e1b102df21533554f458679baebf4
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-08-21 16:35:24 -0400

    updates docs for ticker v2

---
+++ src/routes/(docs)/tickers/v1/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import TryQuery from '$lib/components/TryQuery.svelte';
+</script>
+
+<h2>/v1/</h2>
+
+<p>NOTICE: Penylo will start enforcing versioning on this route August 31st.</p>
+
+<p>
+	If you want a list of all SEC recognized tickers, you can use this api route. Note, it caps the
+	returned tickers at 100
+</p>
+
+<TryQuery responseType="GET" route="https://tickers.penylo.dev/v1/" />
+
+<p>You can use query parameters to search for a specific string.</p>
+<TryQuery responseType="GET" route="https://tickers.penylo.dev/v1/?q=Apple" />
+
+<p>
+	If you need to get more results from the query, you can use the skip header to skip x amount of
+	results. Note: The total records field does not change when using skip.
+</p>
+
+<TryQuery responseType="GET" route="https://tickers.penylo.dev/v1/?q=Apple&skip=2" />
---
+++ src/routes/(docs)/tickers/v2/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import TryQuery from '$lib/components/TryQuery.svelte';
+</script>
+
+<h2>/v2/</h2>
+
+<p>NOTICE: This route is marked as experimental.</p>
+
+<p>With Penylo Tickers v2, we have made searching available.</p>
+<TryQuery responseType="GET" route="https://tickers.penylo.dev/v2/search?q=AAPL" />
+
+<p>Using search is a little slower than using the v1 API for now. It's being worked on.</p>
