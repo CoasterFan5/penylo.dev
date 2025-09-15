3858355468b9350a16f21eed2c818bbb77af4ee7
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-09-15 15:31:13 -0400

    add hidden logo docs

---
+++ src/routes/(docs)/logos/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Link from '$lib/components/Link.svelte';
+</script>
+
+<h2>logo.penylo.dev/::</h2>
+
+<p>Warning: logo.penylo.dev is in very early alpha and not nearing completion</p>
+
+<p>
+	Replace :: with any of the <Link href="/logos/list.json">supported logo keys</Link>. This route
+	returns an svg for the key.
+</p>
---
+++ src/routes/(docs)/logos/list.json/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import TryQuery from '$lib/components/TryQuery.svelte';
+</script>
+
+<h2>logo.penylo.dev/list.json</h2>
+
+<p>This route returns a list of all supported logo keys.</p>
+
+<TryQuery responseType="GET" route="https://logo.penylo.dev/list.json" />
