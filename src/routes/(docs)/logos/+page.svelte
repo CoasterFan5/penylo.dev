513d1007f5e3176403223c0cbb88f0d6ab8a96d5
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-09-16 15:42:16 -0400

    add gallery

--- src/routes/(docs)/logos/+page.svelte
+++ src/routes/(docs)/logos/+page.svelte
@@ -6,6 +6,12 @@
 
 <p>Warning: logo.penylo.dev is in very early alpha and not nearing completion</p>
 
+<h3>Why use Penylo Logos?</h3>
+<p>
+	We don't automatically scrape icons and instead craft each icon by hand to guarentee a good
+	looking svg.
+</p>
+
 <p>
 	Replace :: with any of the <Link href="/logos/list.json">supported logo keys</Link>. This route
 	returns an svg for the key.
---
+++ src/routes/(docs)/logos/gallery/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let items: [string, string][] = $state([]);
+
+	onMount(async () => {
+		const resp = await fetch('https://logo.penylo.dev/list.json');
+		const fetchedItems = (await resp.json()).items as Record<string, string[]>;
+
+		for (const key in fetchedItems) {
+			items.push([key, fetchedItems[key][0]]);
+		}
+		console.log($state.snapshot(items));
+	});
+</script>
+
+<h2>Penylo Logo Gallery ({items.length} items)</h2>
+
+<div class="logos">
+	{#each items as item (item[0])}
+		<img src="https://logo.penylo.dev/{item[1]}" alt="" />
+	{/each}
+</div>
+
+<style lang="scss">
+	.logos {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(3rem, 1fr));
+		gap: 1px;
+		img {
+			width: 100%;
+			height: 100%;
+		}
+	}
+</style>
