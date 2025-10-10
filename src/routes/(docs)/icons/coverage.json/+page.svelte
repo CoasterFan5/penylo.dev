c6537f526c7ee3c17f973b8874778bd08f03226d
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-10-10 13:47:48 -0400

    adds coverage

---
+++ src/routes/(docs)/icons/coverage.json/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	type IconCoverageBody = { count: number; percent: number; details: Record<string, boolean> };
+	const fetchIconCoverage = async () => {
+		const iconRequest = await fetch('https://icons.penylo.dev/coverage.json');
+		return (await iconRequest.json()) as IconCoverageBody;
+	};
+</script>
+
+<h2>Penylo Icons Coverage</h2>
+{#await fetchIconCoverage()}
+	<p>Loading Coverage Data...</p>
+{:then iconCoverage}
+	<p>Serving {iconCoverage.count} icons</p>
+	<p>Covering {iconCoverage.percent.toFixed(2)}% of the S&P 500.</p>
+{/await}
